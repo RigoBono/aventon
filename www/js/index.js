c548e4b67fad3cc4d12799df979fee0350f796c3
Rigo Bono <arigoberto@gmail.com>
2015-11-17 20:00:53 -0600

    Prueba conexion ajax

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,45 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function prueba(){
+/*var arrayResult = mysql_select_query ("SELECT Nombre FROM Usuario WHERE Codigo=209529357");
+for (i=0; i< arrayResult.length; i++) {
+    var fila = arrayResult[i];
+    var columna = arrayResult[i][0];
+}*/
+alert("hola");
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function EnviarDatos(){
+			var codigo=document.getElementById('codigo').value;
+			var nip=document.getElementById('nip').value;
+			
+			$.ajax({
+				type:'POST',
+				url:"envia.php",
+				data:('codigo='+codigo+'&nip='+nip),
+				success:function(respuesta){
+				if(respuesta==1){
+					$('#mensaje').html('Accediste');
+				}
+				else{
+					$('#mensaje').html('nel');
+				}
+				}
+				
+			})
 
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+		}
+		function conecta(){
+		$.ajax({
+				type:'POST',
+				url:"conectaPrueba.php",
+				data:{host:'192.168.0.113',user:'root',pwd:'Rigo1994'},
+				success:function(respuesta){
+				if(respuesta==1){
+					$('#mensaje').html('Accediste :D');
+				}
+				else{
+					$('#mensaje').html(respuesta);;
+				}
+				}
+				
+			})
+		}
